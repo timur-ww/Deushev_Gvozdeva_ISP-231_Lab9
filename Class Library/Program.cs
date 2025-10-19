7ccf6ead4fde4b3f428bb1b480ebf7ca78becf49
timurqqe <timurkkq@gmail.com>
2025-10-19 22:34:26 +0300

    Add files via upload

---
+++ Class Library/Program.cs
@@ -0,0 +1,15 @@
+﻿using MathLibrary;
+
+namespace Class_Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathTools math = new();
+
+            Console.WriteLine($"Add: {math.Add(3, 5)}");
+            Console.WriteLine($"Multiply: {math.Multiply(3, 5)}");
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Delegates {
+    internal class Program {
+        public delegate void MessageHandler(string text);
+        public delegate void NumberHandler(int number);
+        static void Main(string[] args) {
+
+            /*MessageHandler handler = ShowMessage;
+            handler("Привет, мир");*/
+            NumberHandler handler = Double;
+            handler += Square;
+            handler(5);
+        }
+
+        static void ShowMessage(string message) {
+            Console.WriteLine($"Сообщение: {message}");
+        }
+        static void Double(int num) => Console.WriteLine($"Удвоено:  {num * 2}");
+        static void Square(int num) => Console.WriteLine($"Квадрат:  {num * num}");
+
+    }
+}
